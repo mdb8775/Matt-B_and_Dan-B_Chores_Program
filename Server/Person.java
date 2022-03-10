49d767a3533378c7c7663d4e17a12cea8a59903b
Matt Bliss <mdb8775@g.rit.edu>
2022-03-09 20:31:28 -0500

    started person file

---
+++ Server/Person.java
@@ -0,0 +1,17 @@
+"""
+Class file for people for chores list.
+Authors: Matt B and Dan B
+Date: March 9 2022
+"""
+
+public class Person {
+    private final name;
+    private chore1;
+    private chore2;
+    private chore3;
+    private chore4;
+    private chore5;
+    private chore6;
+    private chore7;
+    private chore8;
+}
