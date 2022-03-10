32163687fa4e8ebd295a34e4e30e6dd137ce6424
Matt Bliss <mdb8775@g.rit.edu>
2022-03-09 21:03:32 -0500

    bruhh

--- Server/Person.java
+++ Server/Person.java
@@ -100,9 +100,9 @@ public class Person {
         this.pick_up_kitchen += 1;
     }
 
-public static void main(){
-    System.out.println("Hello World!");
-}
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
 
 }
 
