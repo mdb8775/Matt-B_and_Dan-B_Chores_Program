3cc6ab5f153e3580eb85393b6541dc211c64ad4d
Matt Bliss <mdb8775@g.rit.edu>
2022-03-12 16:55:47 -0500

    stubbed out client file

--- Client/Chores_Client.java
+++ Client/Chores_Client.java
@@ -3,14 +3,44 @@
  * Authors: Matt Bliss and Dan Benedetti
  * Date: March 12, 2022
  */
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Chores_Client {
 
-    //TODO add function to parse CSV file
+    public static ArrayList<Object> parseSheet(String filename) {
+        ArrayList<Object> list = new ArrayList<>();
+        ArrayList<Object> row = new ArrayList<>();
 
-    //TODO 
+        //will need to do this after every row
+            list.add(row);
+
+        return list;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Done.");
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.print("Would you like to use data from a previous Google Sheets file? (y/n) ");
+
+        ArrayList<Object> list;
+
+        String answer = inputScanner.next();
+        System.out.println();
+        if(answer == "y") {
+            System.out.print("Path of the CSV file: ");
+            answer = inputScanner.next();
+
+            //TODO need to either loop and send each list seperately, or need to send list of lists
+            list = parseSheet(answer);
+        }
+
+        //TODO send list to server
+        
+        //TODO wait for server to send chore list back
+
+        //TODO Format and print list
+
+
+        inputScanner.close();
     }
 }
