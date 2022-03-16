12134d7f34f0db0a202f07edbc59f71789e9d47b
Matt Bliss <mdb8775@g.rit.edu>
2022-03-16 14:15:44 -0400

    cleaned up file

--- Chores/Chores_Client.java
+++ Chores/Chores_Client.java
@@ -1,5 +1,8 @@
 /**
- * This will be the client side of the program given to House Manager
+ * The chores program is all one file, where it asks for a CSV file and makes 
+ * people using the Person class, and then makes a list of these people that will
+ * then be assigned chores.
+ * 
  * Authors: Matt Bliss and Dan Benedetti
  * Date: March 12, 2022
  */
@@ -12,7 +15,7 @@ public class Chores_Client {
         ArrayList<Object> list = new ArrayList<>();
         ArrayList<Object> row = new ArrayList<>();
 
-        //will need to do this after every row
+        //TODO will need to do this after every row
             list.add(row);
 
         return list;
@@ -20,23 +23,13 @@ public class Chores_Client {
 
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
-        System.out.print("Would you like to use data from a previous Google Sheets file? (y/n) ");
 
-        ArrayList<Object> list;
+        ArrayList<Object> people_list;
 
-        String answer = inputScanner.next().strip();
-        
-        if(answer.equals("y")) {
-            System.out.print("Path of the CSV file: ");
-            answer = inputScanner.next();
+        System.out.print("Path of the CSV file: ");
+        String filename = inputScanner.next();
 
-            //TODO need to either loop and send each list seperately, or need to send list of lists
-            list = parseSheet(answer);
-        }
-
-        //TODO send list to server
-        
-        //TODO wait for server to send chore list back
+        people_list = parseSheet(filename);
 
         //TODO Format and print list
 
